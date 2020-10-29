d1d31de2dfb6e4704a84ea48ebe2dfa7d5652714
darshana24 <darshana.bagul@codaemonsoftwares.com>
2020-10-29 18:09:01 +0530

    added cart icon in header

--- wp-content/themes/astra-child/header-inner-pages.php
+++ wp-content/themes/astra-child/header-inner-pages.php
@@ -22,13 +22,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <link rel="profile" href="https://gmpg.org/xfn/11">
-
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-    crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
-    crossorigin="anonymous"></script>
 <?php wp_head(); ?>
 </head>
 
@@ -62,16 +55,22 @@ if ( ! defined( 'ABSPATH' ) ) {
 		        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		        <span class="navbar-toggler-icon"></span>
 		      </button>
-		      
-		        <?php 
-		        wp_nav_menu(array(
-		          'theme_location' => 'primary',
-		          'container' => 'div',
-		          'container_class' => 'collapse navbar-collapse', 
-		          'container_id'    => 'navbarSupportedContent',
-		          'menu_class' => '',
-		          'items_wrap' => '<ul class="navbar-nav ml-auto">%3$s</ul>'
-		        ));?>
+		      <?php if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+                $count = WC()->cart->cart_contents_count;
+              } 
+      		  $url = WC()->cart->get_cart_url();
+              wp_nav_menu(array(
+                'theme_location' => 'primary',
+                'container' => 'div',
+                'container_class' => 'collapse navbar-collapse', 
+                'container_id'    => 'navbarSupportedContent',
+                'menu_class' => '',
+                'items_wrap' => '<ul class="navbar-nav ml-auto">%3$s <li>
+                    <span class="cart-icon"><a href="'.$cart.'">'.$count.'</a>
+                    </span>
+                  </li></ul>'
+              ));
+            ?>
 		    </nav>
 		</div>
   </header>
