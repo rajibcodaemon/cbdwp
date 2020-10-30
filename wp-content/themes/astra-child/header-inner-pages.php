ee0e88827f02e78f48c6571f75408fa6b77154c3
darshana24 <darshana.bagul@codaemonsoftwares.com>
2020-10-30 13:57:49 +0530

    added cart url in header pages

--- wp-content/themes/astra-child/header-inner-pages.php
+++ wp-content/themes/astra-child/header-inner-pages.php
@@ -58,7 +58,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 		      <?php if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
                 	$count = WC()->cart->cart_contents_count;
 	              } 
-	              
+	              $url = wc_get_cart_url();
 	              wp_nav_menu(array(
 	                'theme_location' => 'primary',
 	                'container' => 'div',
@@ -66,9 +66,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 	                'container_id'    => 'navbarSupportedContent',
 	                'menu_class' => '',
 	                'items_wrap' => '<ul class="navbar-nav ml-auto">%3$s <li>
-	                    <span class="cart-icon"><a href="#">'.$count.'</a>
-	                    </span>
-	                  </li></ul>'
+	                    <span class="cart-icon"><a href="'.$url.'">'.$count.'</a>
+                    </span>
+                  </li></ul>'
 	              ));
             ?>
 		    </nav>
