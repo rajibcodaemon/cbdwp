17c63980dce0b96c622008fd5dd9d3b04fd4fd04
darshana24 <darshana.bagul@codaemonsoftwares.com>
2020-11-24 21:26:23 +0530

    added header files

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -22,8 +22,8 @@ function child_enqueue_styles() {
 	wp_enqueue_style( 'astra-child-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_ASTRA_CHILD_VERSION, 'all' );
     wp_enqueue_style('font-awesome-min-css', get_stylesheet_directory_uri() . '/assets/css/font-awesome.min.css', array());
     wp_enqueue_style('style-css', get_stylesheet_directory_uri() . '/assets/css/style.css', array());
-    wp_enqueue_script('jquery-slim-js', get_stylesheet_directory_uri() . '/assets/js/jquery-3.5.1.slim.min.js',
-        array('jquery'),'',true);
+    /*wp_enqueue_script('jquery-slim-js', get_stylesheet_directory_uri() . '/assets/js/jquery-3.5.1.slim.min.js',
+        array('jquery'),'',true); */
     wp_enqueue_script('bootstrap-bundle-js', get_stylesheet_directory_uri() . '/assets/js/bootstrap.bundle.min.js',
         array('jquery'),'',true);
 
